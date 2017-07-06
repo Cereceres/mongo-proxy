d3d7d0c67f4ab9e95e024f35714358b79f57e7f2
cereceres <jesus.edelcereceres@gmail.com>
2017-07-06 18:46:38 -0500

    the error with response is fixed

--- test/insert.test.js
+++ test/insert.test.js
@@ -29,7 +29,7 @@ describe('description', () => {
 
 
     it('should', function *() {
-        const reponse = yield this.agent
+        const response = yield this.agent
             .put('/user')
             .send({ test:'test 2' })
             .query(this.query)
