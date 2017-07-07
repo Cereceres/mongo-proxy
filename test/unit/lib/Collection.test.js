03bdb9d245989dadfd8f0900ffd15e14d633288b
cereceres <jesus.edelcereceres@gmail.com>
2017-07-07 14:17:07 -0500

    the find mehtod use a promise syntaxis

--- test/unit/lib/Collection.test.js
+++ test/unit/lib/Collection.test.js
@@ -2,8 +2,21 @@ const assert = require('assert');
 
 const Collection = require('../../../lib/Collection');
 
+const database = {
+    find: () => ({ exec:() => Promise.resolve({ res:'res' }) }),
+    update: (query, cb) => cb('error', { res:'result' }),
+    create: (query, cb) => cb('error', { res:'result' }),
+    find: (query, cb) => cb('error', { res:'result' })
+};
+
+const collection = new Collection(database);
+
 describe('Collection test', () => {
     it('should be a collection', () => {
         assert(new Collection() instanceof Collection);
+    });
+
+    it('should have a find method', () => {
+        assert(typeof collection.find === 'function');
     });
 });
