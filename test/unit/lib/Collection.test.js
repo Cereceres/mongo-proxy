5d0d4a74e3c9223a6d828d2dc9388e451080249a
cereceres <jesus.edelcereceres@gmail.com>
2017-07-07 14:25:26 -0500

    the test to Collection is added

--- test/unit/lib/Collection.test.js
+++ test/unit/lib/Collection.test.js
@@ -2,21 +2,22 @@ const assert = require('assert');
 
 const Collection = require('../../../lib/Collection');
 
-const database = {
-    find: () => ({ exec:() => Promise.resolve({ res:'res' }) }),
-    update: (query, cb) => cb('error', { res:'result' }),
-    create: (query, cb) => cb('error', { res:'result' }),
-    find: (query, cb) => cb('error', { res:'result' })
-};
-
-const collection = new Collection(database);
 
 describe('Collection test', () => {
     it('should be a collection', () => {
         assert(new Collection() instanceof Collection);
     });
 
-    it('should have a find method', () => {
-        assert(typeof collection.find === 'function');
+    it('should have a find method', function *() {
+        const query = { test:'test' };
+        const database = {
+            find: (_query) => ({ exec:() => {
+                assert.deepEqual(query, _query);
+                return Promise.resolve({ res:'res' });
+            } }),
+        };
+        const collection = new Collection(database);
+        const resQuery = yield collection.find(query);
+        assert.deepEqual(resQuery, { res:'res' });
     });
 });
