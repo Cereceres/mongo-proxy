c9674b331cc4c25dac8f9c2a8d5271d70fa181a1
cereceres <jesus.edelcereceres@gmail.com>
2017-07-10 13:21:24 -0500

    the error in test is fixed

--- test/service/complete-flow.test.js
+++ test/service/complete-flow.test.js
@@ -12,8 +12,8 @@ describe('description', () => {
             .send(data)
             .expect('Content-Type', 'application/json')
             .expect(200);
-        assert(res.body.test === data.test);
-        assert(res.body.user === data.user);
+        assert(res.body.$data.test === data.test);
+        assert(res.body.$data.user === data.user);
     });
 
     it('should get the doc created', function *() {
@@ -22,21 +22,21 @@ describe('description', () => {
             .query(this.query)
             .expect('Content-Type', 'application/json')
             .expect(200);
-        assert(res.body[0].test === data.test);
-        assert(res.body[0].user === data.user);
+        assert(res.body.$data[0].test === data.test);
+        assert(res.body.$data[0].user === data.user);
         assert(res.body.length === 1);
     });
 
 
     it('should update the doc created', function *() {
-        const response = yield this.agent
+        yield this.agent
             .put('/user')
             .send({ test:'test 2' })
             .query(this.query)
             .expect('Content-Type', 'application/json')
             .expect(200);
         const res = yield this.collection.find(this.query);
-        console.log('res ===', res, response.body);
+        console.log('res ===', res);
         assert(res[0].get('test') === 'test 2');
         assert(res[0].get('user') === data.user);
     });
