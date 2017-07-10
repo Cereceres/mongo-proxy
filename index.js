89b0bd4b4282ac55f63ceb18dad5b6eaaf0930c0
cereceres <jesus.edelcereceres@gmail.com>
2017-07-10 12:57:21 -0500

    the nyc is removed

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ const databaseUrlDefault = 'mongodb://127.0.0.1:27017/test';
 const portDefault = process.env.PORT || '8080';
 const dbDefault = getDatabase(databaseUrlDefault);
 
-module.exports = (database = dbDefault, port = portDefault) => {
+const getServer = module.exports = (database = dbDefault, port = portDefault) => {
     const getCollection = getterOfCollection(database);
     const cb = getCb(getCollection, database);
     const server = http.createServer(cb);
     server.listen(port);
     return server;
 };
+
+if (!module.parent) getServer();
+
