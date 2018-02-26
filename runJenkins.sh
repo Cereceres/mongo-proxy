fba1b86825d653a1e7719620eb82bee52105a367
cereceres <jesus.edelcereceres@gmail.com>
2018-02-26 16:07:41 -0600

    nvm is added

--- runJenkins.sh
+++ runJenkins.sh
@@ -1,3 +1,7 @@
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 6
+nvm use 6
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 export PATH=~/.npm-global/bin:$PATH
